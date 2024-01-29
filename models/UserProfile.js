30bd4ad4cc253137e5e42870735e4ff1fc3553cc
DnMakyr <107171443+NguyenLDHE176088@users.noreply.github.com>
2024-01-30 01:10:48 +0700

    added models for user, profile and post

--- models/UserProfile.js
+++ models/UserProfile.js
@@ -1,8 +1,51 @@
-/** 
- * @des 
- * @author 
- * @date 
+import mongoose from "mongoose";
+/**
+ * @des model for user profile
+ * @author Nguyen
+ * @date 30/01/2024
  * @param {*} req
  * @param {*} res
  * @returns
  */
+const UserProfileSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    fullname: {
+      type: String,
+      required: true,
+    },
+    avatar_url: {
+      type: String,
+      required: true,
+    },
+    bio: {
+      type: String,
+      required: false,
+    },
+    connections: [
+      {
+        userId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+        },
+        status: {
+          type: string,
+          default: 0,
+        },
+      },
+    ],
+    posts: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Post",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const UserProfile = mongoose.model("UserProfile", UserProfileSchema);
+export default UserProfile;
